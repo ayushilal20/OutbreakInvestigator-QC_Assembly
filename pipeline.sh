98f04cc15838e3eeb3895873149aedbb89c9e226
Bindra, Mehak <mbindra3@gatech.edu>
2024-02-20 17:44:23 -0500

    Create pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -15,28 +15,34 @@ conda deactivate
 input_dir="$1"
 output_dir="$2"
 
-cd $input_dir
-fastqc *
-multiqc .
-
-cd ..
-bash trimm.sh
-cd reports
-multiqc .
-
-# phix removal
-bash bbduk.sh
-
 for file in "$input_dir"/*R1*; do
 	R1=$(basename -- "$file")
 	R2="${filename/R1/R2}"
 	# isolate name saved as $isolate
 	isolate="${R1:0:8}"
 
+	# create output dirs for fastp, bbduk, skesa and quast
+	mkdir -p "$output_dir"/{fastp,reports,bbduk,skesa,quast}/$isolate
+
+	#trimming with fastp
+	fastp -w 6 \
+	--qualified_quality_phred 30 \
+	--unqualified_percent_limit 20 \
+	--dont_eval_duplication \
+	-i $R1 -I $R2 \
+	-o "$output_dir/fastp_$isolate/trimmed_${R1}" \
+	-O "$output_dir/fastp_$isolate/trimmed_${R2}" \
+	-h "$output_dir/reports/${isolate}_fastp_report.html" \
+	-j "$output_dir/reports/${isolate}_fastp.json" -z 6
+
+	#bbduk
+	 bbduk.sh in="$output_dir/fastp_$isolate/trimmed_${R1}" out="$output_dir/bbduk_$isolate/unmatched_${R1}" outm="$output_dir/bbduk_$isolate/matched_${R1}" ref=phix k=31 hdist=1 overwrite=t stats="$output_dir/bbduk_$isolate/R1_stats.txt" 
+    	 bbduk.sh in="$output_dir/fastp_$isolate/trimmed_${R2}" out="$output_dir/bbduk_$isolate/unmatched_${R2}" outm="$output_dir/bbduk_$isolate/matched_${R2}" ref=phix k=31 hdist=1 overwrite=t stats="$output_dir/bbduk_$isolate/R2_stats.txt" 
+	 
 	#assembly with skesa
 	skesa \
-	--fastq "$output_dir/fastp_$isolate/trimmed_${R1}" \
-	"$output_dir/fastp_$isolate/trimmed_${R2}" \
+	--fastq "$output_dir/bbduk_$isolate/matched_${R1}" \
+	"$output_dir/bbduk_$isolate/matched_${R2}" \
 	--contigs_out "$output_dir/skesa_$isolate/$isolate.fasta" \
 	1> "$output_dir/skesa_$isolate"/skesa.stdout.txt \
 	2> "$output_dir/skesa_$isolate"/skesa.stderr.txt
@@ -48,6 +54,6 @@ for file in "$input_dir"/*R1*; do
 		-o "$output_dir/skesa_$isolate/filtered_$isolate.fna"
 	conda deactivate
 
+	#quality with quast
+
 done
-
-
