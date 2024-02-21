7210e877b8027fa5ddbeeb767ec72efeb355ec72
Bindra, Mehak <mbindra3@gatech.edu>
2024-02-20 21:00:46 -0500

    Update velvet.sh

--- velvet.sh
+++ velvet.sh
@@ -8,8 +8,8 @@ for r1 in "$reads_dir"/*_R1.fq.gz; do
     if [[ -f $r2 ]] ; then
 	contigs="$assembly_dir"/"$(echo "$r1" | sed 's/^.*_\(F[0-9]*\)_.*/\1/')"
 	mkdir -p "$contigs"
-	velveth "$contigs" 31 -shortPaired -fastq "$r1" "$r2" 1> "$contigs"/velvet.stdout.txt 2> "$contigs"/velvet.stderr.txt
-	velvetg "$contigs" -cov_cutoff auto 1> "$contigs"/velvet.stdout.txt 2> "$contigs"/velvet.stderr.txt
+	velveth "$contigs" 31 -shortPaired -fastq "$r1" "$r2" 1> "$contigs"/velveth.stdout.txt 2> "$contigs"/velveth.stderr.txt
+	velvetg "$contigs" -cov_cutoff auto 1> "$contigs"/velvetg.stdout.txt 2> "$contigs"/velvetg.stderr.txt
     else
         echo "$r2 not found" >&2
     fi
